f9fb296440c0c920ac2b751ed30fd2d4dda2b8de
Matthew Hicks <mkigikm@gmail.com>
2015-02-16 12:20:53 -0500

    removes redudant auth specs

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -8,34 +8,17 @@ feature "the signup process" do
   end
 
   feature "signing up a user" do
-    it "requires a name" do
+    it "requires a valid user" do
       visit new_user_url
       click_on "Sign Up"
 
       expect(page).to have_content("Name can't be blank")
     end
 
-    it "requires a minimum password length" do
-      visit new_user_url
-      fill_in "Name", with: "matt"
-      fill_in "Password", with: "abcde"
-      click_on "Sign Up"
-
-      expect(page).to have_content("Password is too short")
-    end
-
     it "show username on the homepage after signup" do
       sign_up("matt")
 
       expect(page).to have_content("matt")
-    end
-
-    it "requires unique usernames" do
-      sign_up("matt")
-      click_on "Sign Out"
-      sign_up("matt")
-
-      expect(page).to have_content("Name has already been taken")
     end
 
     it "doesn't allow signed in users access to the new user page" do
